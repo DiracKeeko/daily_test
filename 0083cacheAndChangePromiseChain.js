5f58a1caa7066b0c46a341e752a85c5b09cd31e1
Keeko <787018661@qq.com>
2022-06-17 11:00:15 +0800

    [feat] 0083cacheAndChangePromiseChain.js

---
+++ 0083cacheAndChangePromiseChain.js
@@ -0,0 +1,66 @@
+import { postFormInfo } from "@/api/modules/baseOnAxios.js";
+import { getHash } from "@/util/acquire.js";
+
+// ↓ 处理resData
+function handleRes(resData) {
+  // ...
+  return resData;
+}
+
+function state() {
+  return {
+    httpRes: {} // ← 这里是缓存数据的地方
+    /* 
+    // httpRes的具体形式
+    httpRes: {
+      hash1: promise1,
+      hash2: promise2,
+      ...
+    }
+    */
+  };
+}
+
+
+const mutations = {
+  SET_ONCE_INVEST_RES(s, { httpParamhash, resPromise }) {
+    s.httpRes[`${httpParamhash}`] = resPromise;
+  },
+  DELETE_ONCE_INVEST_RES(s, { httpParamhash }) {
+    delete s.httpRes[`${httpParamhash}`];
+  }
+};
+
+const actions = {
+  async ASYNC_GET_ONCE_REQ({ state, commit }, param) {
+    const httpParamhash = getHash(param);
+    try {
+      if (state.httpRes[`${httpParamhash}`]) {
+        return await state.httpRes[`${httpParamhash}`];
+      }
+      
+      const resPromise = new Promise((resolve, reject) => {
+        postFormInfo(param)
+          .then(resData => {
+            const finalRes = handleRes(resData);
+            resolve(finalRes);
+          })
+          .catch(err => {
+            reject(err);
+          });
+      });
+      commit("SET_ONCE_INVEST_RES", { httpParamhash, resPromise });
+      return await resPromise;
+    } catch (err) {
+      commit("DELETE_ONCE_INVEST_RES", { httpParamhash });
+    }
+    return null;
+  }
+};
+
+export default {
+  namespaced: true,
+  state,
+  mutations,
+  actions
+};

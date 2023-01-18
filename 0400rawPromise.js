81cbdc231f21f2ffd6b019cceaa51455105309b8
Keeko <787018661@qq.com>
2023-01-18 17:15:29 +0800

    [feat] 手写promise 03 处理then(arg1, arg2)不是函数的情况

--- 0400rawPromise.js
+++ 0400rawPromise.js
@@ -1,13 +1,15 @@
 let promise = new Promise((resolve, reject) => {
-  // resolve("这次一定");
-  // reject("下次一定");
-  throw new Error("洗洗睡吧");
+  resolve("这次一定");
+  reject("下次一定");
+  // throw new Error("洗洗睡吧");
 });
 
 promise.then(
-  (result) => {
-    console.log(result);
-  },
+  // ↓ 原生Promise中，传入的参数如果不是函数就要被忽略
+  // (result) => {
+  //   console.log(result);
+  // },
+  undefined,
   (result) => {
     console.log(result.message);
   }
--- 0401buildPromise.js
+++ 0401buildPromise.js
@@ -6,7 +6,8 @@ class Commitment {
   constructor(func) {
     this.status = Commitment.PENDING;
     this.result = null;
-    try { // 02 处理创建实例时 throw new Error
+    try {
+      // 02 处理创建实例时 throw new Error
       // func(this.resolve.bind, this.reject); // 报错,status为undefined, 说明this跟丢了
       func(this.resolve.bind(this), this.reject.bind(this)); // 01 处理this指向
     } catch (error) {
@@ -21,12 +22,16 @@ class Commitment {
     }
   }
   reject(result) {
+    console.log("result in reject->", result);
     if (this.status === Commitment.PENDING) {
       this.status = Commitment.REJECTED;
       this.result = result;
     }
   }
   then(onFULFILLED, onREJECTED) {
+    // 03 处理then的参数不是function的情况
+    onFULFILLED = typeof onFULFILLED === "function" ? onFULFILLED : () => {}; // 如果不是function则
+    onREJECTED = typeof onREJECTED === "function" ? onREJECTED : () => {};
     if (this.status === Commitment.FULFILLED) {
       onFULFILLED(this.result);
     }
@@ -37,14 +42,15 @@ class Commitment {
 }
 
 let commitment = new Commitment((resolve, reject) => {
-  // resolve("这次一定");
-  throw new Error("洗洗睡吧");
+  resolve("这次一定");
+  // throw new Error("洗洗睡吧");
 });
 
 commitment.then(
-  (result) => {
-    console.log(result);
-  },
+  // (result) => {
+  //   console.log(result);
+  // },
+  undefined,
   (result) => {
     console.log(result.message);
   }

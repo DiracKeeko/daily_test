0ae51e17c286d8eb060a77da59e57132926d514d
Keeko <787018661@qq.com>
2023-01-18 17:00:05 +0800

    [feat] 手写promise 02 处理创建实例时throw new Error

--- 0400rawPromise.js
+++ 0400rawPromise.js
@@ -1,3 +1,14 @@
 let promise = new Promise((resolve, reject) => {
-  resolve("这次一定");
+  // resolve("这次一定");
+  // reject("下次一定");
+  throw new Error("洗洗睡吧");
 });
+
+promise.then(
+  (result) => {
+    console.log(result);
+  },
+  (result) => {
+    console.log(result.message);
+  }
+);
--- 0401buildPromise.js
+++ 0401buildPromise.js
@@ -6,21 +6,46 @@ class Commitment {
   constructor(func) {
     this.status = Commitment.PENDING;
     this.result = null;
-    // func(this.resolve.bind, this.reject); // 报错,status为undefined, 说明this跟丢了
-    func(this.resolve.bind(this), this.reject.bind(this)); // 01处理this指向
-  }
-  resolve() {
-    if (this.status === Commitment.PENDING) {
-      this.status = Commitment.FULFILLED;
+    try { // 02 处理创建实例时 throw new Error
+      // func(this.resolve.bind, this.reject); // 报错,status为undefined, 说明this跟丢了
+      func(this.resolve.bind(this), this.reject.bind(this)); // 01 处理this指向
+    } catch (error) {
+      this.reject(error); // 这里是直接执行, 不是创建实例后再执行, 因此不用bind
     }
   }
-  reject() {
+  resolve(result) {
+    console.log("result in resolve->", result);
+    if (this.status === Commitment.PENDING) {
+      this.status = Commitment.FULFILLED;
+      this.result = result;
+    }
+  }
+  reject(result) {
     if (this.status === Commitment.PENDING) {
       this.status = Commitment.REJECTED;
+      this.result = result;
+    }
+  }
+  then(onFULFILLED, onREJECTED) {
+    if (this.status === Commitment.FULFILLED) {
+      onFULFILLED(this.result);
+    }
+    if (this.status === Commitment.REJECTED) {
+      onREJECTED(this.result);
     }
   }
 }
 
 let commitment = new Commitment((resolve, reject) => {
-  resolve("这次一定");
+  // resolve("这次一定");
+  throw new Error("洗洗睡吧");
 });
+
+commitment.then(
+  (result) => {
+    console.log(result);
+  },
+  (result) => {
+    console.log(result.message);
+  }
+);

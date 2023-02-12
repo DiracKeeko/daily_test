1d3598d6a698e7d5514cdfad4ccd92eac5b1c3f4
keeko <787018661@qq.com>
2023-02-12 20:59:09 +0800

    Array.includes test

---
+++ 0092includes-test.js
@@ -0,0 +1,7 @@
+const strArr1 = ["aaa", "bbb"];
+const res = strArr1.includes("aaa");
+
+console.log("res->", res); // true
+
+const res1 = strArr1.includes("a");
+console.log("res1->", res1); // false

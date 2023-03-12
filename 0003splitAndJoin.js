bea230b4e23e8ac487517b563fad100871d2ea10
keeko <787018661@qq.com>
2023-03-12 10:38:18 +0800

    [chore] split、join

---
+++ 0003splitAndJoin.js
@@ -0,0 +1,21 @@
+let str = "a bc def gggg";
+let arr = str.split();
+console.log("arr->", arr); // split-> [ 'a bc def gggg' ]
+let arr1 = str.split(" ");
+console.log("arr1->", arr1); // arr1-> [ 'a', 'bc', 'def', 'gggg' ]
+
+let arr2 = "abc".split("");
+console.log("arr2->", arr2); // arr2-> [ 'a', 'b', 'c' ]
+
+str = "abc,bcd,efg,hij";
+let result = str.split(",");
+console.log("result->", result); // result-> [ 'abc', 'bcd', 'efg', 'hij' ]
+
+
+// arr.join(); join有默认值","
+const baseArr = ["a", "b", "c"];
+const joinRes1 = baseArr.join(); // 等价于baseArr.join(",");
+console.log("joinRes1->", joinRes1); // "a,b,c"
+
+const joinRes2 = baseArr.join("");
+console.log("joinRes2->", joinRes2); // "abc"

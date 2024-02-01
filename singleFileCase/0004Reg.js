97337f89e81c59f3611eb087ad2c2f675dc1c9ee
Keeko <787018661@qq.com>
2024-02-01 20:31:11 +0800

    reg ?: 使用

--- singleFileCase/0004Reg.js
+++ singleFileCase/0004Reg.js
@@ -9,3 +9,13 @@ const reg2 = /[0-9]{4}/g; // 匹配任意四个连续的数字 全局模式
 const str2 = "ac1233cc8888";
 const res2 = reg2.exec(str2);
 console.log(res2); // [ '1233', index: 2, input: 'ac1233cc8888', groups: undefined ]
+
+
+// const regStartWithAtSame = /^@(\/.*|$)/; 
+// 上下两个正则没什么区别  (?: ...) 表示非捕获型分组
+const regStartWithAt = /^@(?:\/.*|$)/;
+console.log(regStartWithAt.test("@")); // true
+console.log(regStartWithAt.test("@utils")); // false
+console.log(regStartWithAt.test("@/utils")); // true
+console.log(regStartWithAt.test("@/components/common")); // true
+console.log(regStartWithAt.test("@/")); // true

37a13fe76fd2a8d36489a817afdaa1f0b8bfae24
Keeko <787018661@qq.com>
2024-02-04 16:58:02 +0800

    [feat] 文件夹路径和css的正则表达式

--- singleFileCase/0004Reg.js
+++ singleFileCase/0004Reg.js
@@ -20,7 +20,32 @@ console.log(regStartWithAt.test("@/utils")); // true
 console.log(regStartWithAt.test("@/components/common")); // true
 console.log(regStartWithAt.test("@/")); // true
 
+console.log("=============同级文件 ./开头 尾缀不能带.js .ts等 ===========");
+const sameFolderReg = /^(\.\/\w+)(\/\w+)*$/;
+console.log("sameFolderReg->", sameFolderReg.test("file")); // false
+console.log("sameFolderReg->", sameFolderReg.test("./file")); // true
+console.log("sameFolderReg->", sameFolderReg.test("./folder/file")); // true
+console.log("sameFolderReg->", sameFolderReg.test("./folder/folder1/file")); // true
+console.log("sameFolderReg->", sameFolderReg.test("./folder/folder1/012file")); // true
+console.log("sameFolderReg->", sameFolderReg.test("./folder/folder1/file.js")); // false
+
+console.log("=============上级文件 多个(../)开头 尾缀不能带.js .ts等 ===========");
+const highLevelFolderReg = /^(\.\.\/)+\w+(\/\w+)*$/;
+console.log("highLevelFolderReg->", highLevelFolderReg.test("file")); // false
+console.log("highLevelFolderReg->", highLevelFolderReg.test("../file")); // true
+console.log("highLevelFolderReg->", highLevelFolderReg.test("../../folder/file")); // true
+console.log("highLevelFolderReg->", highLevelFolderReg.test("../folder/folder1/file")); // true
+console.log("highLevelFolderReg->", highLevelFolderReg.test("../folder/folder1/012file")); // true
+console.log("highLevelFolderReg->", highLevelFolderReg.test("../folder/folder1/012file.js")); // false
+
 console.log("=============css===========");
-const styleRegLessAndCss = /^.+\.?(less|css)$/;
-console.log(styleRegLessAndCss.test("styles.css")); // true
-console.log(styleRegLessAndCss.test("./test.less")); // true
+const styleRegLessAndCss = /^\.?\.\/\w+(\/\w+)*(\.(less|css))$/;
+console.log("styleRegLessAndCss->", styleRegLessAndCss.test("styles.css")); // false;
+console.log("styleRegLessAndCss->", styleRegLessAndCss.test("styles.less")); // false;
+console.log("styleRegLessAndCss->", styleRegLessAndCss.test("./test.less")); // true
+console.log("styleRegLessAndCss->", styleRegLessAndCss.test("../test.less")); // true
+console.log("styleRegLessAndCss->", styleRegLessAndCss.test("../folder/test.less")); // true
+
+
+const highLevelFolderCSSReg = /^(\.\.\/)+\w+(\/\w+)*(\.(less|css))$/;
+const sameFolderCSSReg = /^(\.\/\w+)(\/\w+)*(\.(less|css))$/;

6956d8c6a036eb9c54f6175b9ecb7c58e51cb022
keeko <787018661@qq.com>
2024-02-28 19:07:25 +0800

    0063组合继承

---
+++ singleFileCase/0063组合继承.js
@@ -0,0 +1,20 @@
+function Parent() {
+  this.sayHello = function () {
+    console.log('Hello');
+  };
+}
+
+Parent.prototype.a = "父类构造函数属性";
+
+function Child() {
+  Parent.call(this);
+}
+
+Child.prototype = new Parent();
+
+const child1 = new Child();
+const child2 = new Child();
+
+console.log(child1.sayHello === child2.sayHello); // false
+console.log("child1.a->", child1.a); // undefined
+

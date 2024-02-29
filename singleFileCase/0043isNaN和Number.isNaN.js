60d4a3442e68e1b90df5173cd5c580d06a44175f
keeko <787018661@qq.com>
2024-02-29 22:36:50 +0800

    Number() undefined, null

--- singleFileCase/0043isNaN和Number.isNaN.js
+++ singleFileCase/0043isNaN和Number.isNaN.js
@@ -9,3 +9,13 @@ console.log(isNaN(123)); // false
 
 console.log(Number.isNaN("123")); // false
 console.log(isNaN("123")); // false
+
+console.log(Number.isNaN(undefined)); // false
+console.log(isNaN(undefined)); // true
+
+console.log(Number.isNaN(null)); // false
+console.log(isNaN(null)); // false
+
+
+console.log(Number(undefined)); // NaN
+console.log(Number(null)); // 0

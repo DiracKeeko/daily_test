6036d32638edb2d81f1b1c02a593a4d2328848c9
keeko <787018661@qq.com>
2024-03-07 17:01:13 +0800

    update 1033weakMap

--- singleFileCase/1033weakMap.js
+++ singleFileCase/1033weakMap.js
@@ -4,9 +4,9 @@
   WeakMap 相较于Map
 
   1. WeakMap只接受引用数据类型作为键名 (object, array, function都可以，但是null不可以)
-  2. 没有遍历操作的API (也不可以直接遍历操作)
-  3. 没有clear清空方法
-  4. WeakMap的键名(key)所指向的对象，一旦不再需要，里面的键名对象和所对应的键值对会自动消失，不用手动删除引用
+  2. WeakMap的键名(key)所指向的对象，一旦不再需要，里面的键名对象和所对应的键值对会自动消失，不用手动删除引用
+  3. 没有遍历操作的API (也不可以直接遍历操作)
+  4. 没有clear清空方法
 */
 
 /* 
@@ -20,13 +20,69 @@
     wm.get(element) // "some information"
 */
 
-const map = new WeakMap();
-let arr = [1, 2, 3];
-map.set(arr, 3);
+const map = new Map();
 
-const res = map.get(arr);
+const key1 = "key1";
+const key2 = 123;
+const key3 = { name: "John" };
+
+map.set(key1, "value1");
+map.set(key2, "value2");
+map.set(key3, "value3");
+
+map.get(key1); // "value1"
+map.get(key2); // "value2"
+
+// 使用forEach遍历
+console.log("forEach->");
+map.forEach((value, key) => {
+  console.log(key, value);
+})
+
+// 使用keys()遍历
+console.log("map.keys()->");
+for (let key of map.keys()) {
+  console.log(key, map.get(key));
+}
+
+// clear()
+map.clear();
+
+
+// WeakMap 
+const weakMap = new WeakMap();
+
+// 不能使用基本数据类型作为key，否则会直接报错。
+// weakMap.set(key1, "value1"); // TypeError: Invalid value used as weak map key
+// weakMap.set(key2, "value2"); // TypeError: Invalid value used as weak map key
+weakMap.set(key3, "value3");
+
+
+let arr = [1, 2, 3];
+weakMap.set(arr, 3);
+
+// weakMap使用forEach遍历，会抛出异常 TypeError: weakMap.forEach is not a function
+/* 
+weakMap.forEach((value, key) => {
+  console.log(key, value);
+})
+ */
+
+// weakMap 使用keys()遍历  TypeError: weakMap.keys is not a function or its return value is not iterable
+/* 
+for (let key of weakMap.keys()) {
+  console.log(key, weakMap.get(key));
+}
+ */
+
+const res = weakMap.get(arr);
 console.log("res->", res); // 3
 
 arr = null;
-const res2 = map.get(arr);
+const res2 = weakMap.get(arr);
 console.log("res2->", res2); // undefined 自动删除了
+
+
+// clear()
+// weakMap.clear(); // TypeError: weakMap.clear is not a function
+

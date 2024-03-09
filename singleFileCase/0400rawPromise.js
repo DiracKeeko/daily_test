813d4dc8bcb819606efdaedce75da037bcbff8c0
keeko <787018661@qq.com>
2024-03-09 11:53:10 +0800

    update promise

--- singleFileCase/0400rawPromise.js
+++ singleFileCase/0400rawPromise.js
@@ -1,22 +1,47 @@
-console.log("01");
+console.log('01');
 let promise = new Promise((resolve, reject) => {
-  console.log("02");
+  console.log('02');
+  /* setTimeout(() => {
+    resolve('这次一定');
+    console.log('04');
+  }, 200); */
+
   setTimeout(() => {
-    resolve("这次一定");
-    console.log("04");
-  })
+    reject('错了');
+    console.log('05');
+    throw new Error("洗洗睡吧");
+  }, 200);
   // reject("下次一定");
   // throw new Error("洗洗睡吧");
 });
 
-promise.then(
+/* promise.then(
   // ↓ 原生Promise中，传入的参数如果不是函数就要被忽略 (如undefined)
   (result) => {
+    console.log('进入resolve');
     console.log(result);
   },
-  (result) => {
-    console.log(result.message);
+  (res) => {
+    console.log('进入reject');
+    console.log(res);
   }
-);
+); */
 
-console.log("03");
+promise
+  .then(
+    // ↓ 原生Promise中，传入的参数如果不是函数就要被忽略 (如undefined)
+    (result) => {
+      console.log('进入resolve');
+      console.log(result);
+    },
+    (res) => {
+      console.log('进入reject');
+      console.log(res);
+    }
+  )
+  .catch((err) => {
+    console.log('进入catch');
+    console.log(err);
+  });
+
+console.log('03');

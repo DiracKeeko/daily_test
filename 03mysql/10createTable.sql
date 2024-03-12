f44e0c3ac040702d3411a851141fbf0d4d935f59
keeko <787018661@qq.com>
2024-03-12 14:44:30 +0800

    mysql change drop

--- 03mysql/10createTable.sql
+++ 03mysql/10createTable.sql
@@ -5,8 +5,7 @@ CREATE TABLE `system_notice` (
     `content` VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL COMMENT '通知内容',
     `user_id` INT NOT NULL COMMENT '通知创建人id',
     `user_name` VARCHAR(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL COMMENT '通知创建人name',
-    `publish_state` TINYINT(1) NOT NULL DEFAULT 0 COMMENT '发布状态: 0未发布, 1已发布, 2已下线',
-    `state` TINYINT(1) NOT NULL DEFAULT 0 COMMENT '删除状态 0:未删除, 1:已删除',
+    `state` TINYINT(1) NOT NULL DEFAULT 0 COMMENT '状态: 0未发布, 1已发布, 2已下线, 3已删除',
     `create_time` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
     `update_time` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
     PRIMARY KEY (`id`)
--- 03mysql/20modifyTable.sql
+++ 03mysql/20modifyTable.sql
@@ -1,4 +1,16 @@
 ALTER TABLE system_notice
 ADD COLUMN skin TINYINT(1) DEFAULT 1 COMMENT '皮肤类别: 0默认, 1喜庆, 2悲伤',
-ADD COLUMN release_time DATETIME COMMENT '发布时间',
-ADD COLUMN expire_time DATETIME COMMENT '过期时间';
+ADD COLUMN publish_time DATETIME COMMENT '发布时间',
+
+
+ALTER TABLE system_notice MODIFY COLUMN notice_type TINYINT(1) DEFAULT 1 NOT NULL COMMENT '通知类型: 1纯文本, 2纯图片';
+-- 上下两行等价
+ALTER TABLE system_notice CHANGE COLUMN notice_type notice_type TINYINT(1) DEFAULT 1 NOT NULL COMMENT '通知类型: 1纯文本, 2纯图片';
+
+
+-- 在sql的change语句中，表名不需要加引号，即使是'state'也不需要加引号
+ALTER TABLE system_notice CHANGE COLUMN state state TINYINT(1) DEFAULT 0 NOT NULL COMMENT '状态 0:未发布, 1:已发布, 2:已下线, 3:已删除';
+
+
+-- 修改表的字段 
+MODIFY COLUMN update_time DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT '更新时间';

5002b01a949e30dc5f3f678e0e36cd401522d195
keeko <787018661@qq.com>
2024-03-22 14:57:19 +0800

    update arrowFunc arguments

--- singleFileCase/0096arrowFunc箭头函数.js
+++ singleFileCase/0096arrowFunc箭头函数.js
@@ -8,3 +8,13 @@ function foo() {
 
 global.id = 22;
 foo.call({ id: 123 })
+
+
+function foo1() {
+  const inner = () => {
+     console.log("args:", arguments);
+  };
+  inner();
+}
+
+foo1(1, 2, 3); // args: [Arguments] { '0': 1, '1': 2, '2': 3 }

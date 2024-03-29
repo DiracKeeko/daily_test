fd0bd182515bc1a0637a50b32607c04edc234bd1
keeko <787018661@qq.com>
2024-03-29 17:29:40 +0800

    [feat] 重置auto_increment & 删除表中确定行

---
+++ 03mysql/22modifyTable_resetAutoIncrement.sql
@@ -0,0 +1,4 @@
+-- 重置表中的 AUTO_INCREMENT 字段
+ALTER TABLE user_info_d AUTO_INCREMENT = 1;
+
+-- 下一个auto_increment字段可用的值将从1开始 (插入，就是1)
---
+++ 03mysql/32deleteTable.sql
@@ -0,0 +1,6 @@
+-- 删除数据库daily_test 中的user_info_d表中的 id = 3 的用户
+DELETE FROM user_info_d WHERE id = 3;
+
+-- 删除数据库daily_test 中的user_info_d表中的 id = 3 的用户
+DELETE FROM user_info_d WHERE unique_id = 008;
+

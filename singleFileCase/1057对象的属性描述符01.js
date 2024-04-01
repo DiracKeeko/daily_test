3d0f3a1afa831e3a8e5216255d81bcdbf0598b00
keeko <787018661@qq.com>
2024-04-01 17:22:51 +0800

    属性描述符value使用

---
+++ singleFileCase/1057对象的属性描述符01.js
@@ -0,0 +1,10 @@
+// 对象的 属性描述符
+
+let obj = {}; // 空对象
+Object.defineProperty(obj, "age", { value: 18 }); // "value"  是属性描述符
+
+console.log(obj.age); // 18
+console.log(Object.getOwnPropertyDescriptor(obj, "age")); 
+// { value: 18, writable: false, enumerable: false, configurable: false }
+
+console.log(Object.getOwnPropertyDescriptor(obj, "age").value); // 18

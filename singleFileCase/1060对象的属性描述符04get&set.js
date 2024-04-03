58fd57df0e812289d1c75849fb979687ce23ea43
keeko <787018661@qq.com>
2024-04-03 16:09:00 +0800

    对象字面量 & get, set 操作

--- singleFileCase/1060对象的属性描述符04get&set.js
+++ singleFileCase/1060对象的属性描述符04get&set.js
@@ -25,3 +25,29 @@ console.log("change->", obj.age); // change-> 28
   思考：
     let obj = Object.create(Object.prototype, {}) 与 let obj = {} 创建对象有什么不同？
 */
+
+// get set 简化写法 结合对象字面量
+let obj1 = {
+  _age: 18,
+  _name: "Rookie",
+  // ↓ 访问age属性的时候，会触发 get age()方法; 这里age虽然像是函数，但是使用的时候是当做属性来使用的。
+  get age() {
+    return this._age;
+  },
+  set age(val) {
+    this._age = val;
+  },
+  get name() {
+    return this._name;
+  },
+  set name(val) {
+    this._name = val;
+  }
+}
+
+console.log("obj1._age->", obj1._age); // 18
+console.log(obj1.age); // 18
+console.log(obj1.name); // Rookie
+
+obj1.age = 28;
+console.log("obj1-change->", obj1.age); // change-> 28

e18564f50fc60d2dd321132c3718e1469ade55ca
keeko <787018661@qq.com>
2024-04-05 12:46:12 +0800

    手写柯里化 传参过程

--- singleFileCase/0100函数柯里化curry.js
+++ singleFileCase/0100函数柯里化curry.js
@@ -6,13 +6,15 @@ function sum(a, b, c) {
 
 function curry(fn) {
   return function curried(...args) {
+    console.log("args->", args);
     if (args.length >= fn.length) {
       // 如果参数的个数达到length，执行原函数
       return fn.apply(this, args);
     } else {
       // 如果参数的个数没有达到length，返回一个新的函数
       return function(...args2) {
-        return curried.apply(this, args.concat(args2)) 
+        console.log("args2->", args2);
+        return curried.apply(this, [...args, ...args2]);
       }
     }
   }

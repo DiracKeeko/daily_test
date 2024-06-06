23db438df0f104701c38aeb7367d818b9a079a51
Keeko <787018661@qq.com>
2024-06-06 11:12:12 +0800

    for循环 + setTimeout

--- singleFileCase/0012setTimeout.js
+++ singleFileCase/0012setTimeout.js
@@ -1,3 +1,4 @@
+/* 
 console.log('001');
 setTimeout(function () {
   console.log('002');
@@ -14,12 +15,38 @@ setTimeout(consoleMsg, 100, '004');
 setTimeout(() => {
   consoleMsg('005');
 }, 100);
+ */
 
 /* 
+  上面代码块的执行顺序
+
   从前到后
   001
   003
   002
   004
   005
+*/
+
+
+// for循环中的setTimeout (for循环 + setTimeout)
+for (let i = 0; i < 3; i++) {
+  setTimeout(() => {
+    console.log(i); // i === 3?
+    console.log(0);
+  }, 100)
+  console.log(5);
+}
+
+/* 
+  执行顺序从前到后
+  5
+  5
+  5
+  0
+  0
+  1
+  0
+  2
+  0
 */

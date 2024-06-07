172979289402d1e183a9cd3906f08327d84e3369
keeko <787018661@qq.com>
2024-06-07 20:17:47 +0800

    多个key排序的实现

---
+++ common/mockData/testData.js
@@ -0,0 +1,382 @@
+const needSort = [{
+  "_id": {
+    "$oid": "5fc620a9194681427cd36e81"
+  },
+  "fundName": "博时安盈A",
+  "fundId": "000084.OF",
+  "fundSecKind": "短期纯债型",
+  "fundManager": "黄海峰",
+  "fundCompany": "博时基金",
+  "issueDate": {
+    "$date": "2013-04-22T15:59:17Z"
+  },
+  "benchmark": "中国人民银行公布的1年期定期存款利率(税后)",
+  "curScale": 6119323079.44,
+  "avgScale": 9970827467.62666,
+  "lastWeekYield": 0.0005,
+  "lastMonthYield": 0.002,
+  "lastYearYield": 0.032,
+  "lastQuarterYield": 0.0062,
+  "yieldInThisYear": 0.0222,
+  "yieldFromCreate": 0.0262,
+  "windStar": 5,
+  "szStar": 5
+},{
+  "_id": {
+    "$oid": "5fc620a9194681427cd36e82"
+  },
+  "fundName": "招商安润",
+  "fundId": "000126.OF",
+  "fundSecKind": "灵活配置型",
+  "fundManager": "尹晓红，钟贇",
+  "fundCompany": "招商基金",
+  "issueDate": {
+    "$date": "2013-04-18T15:59:17Z"
+  },
+  "benchmark": "沪深300指数收益率*55%+中债综合全价(总值)指数收益率*45%",
+  "curScale": 259458288.54,
+  "avgScale": 226237247.603333,
+  "lastWeekYield": -0.0033,
+  "lastMonthYield": -0.0182,
+  "lastYearYield": 0.7453,
+  "lastQuarterYield": 0.2024,
+  "yieldInThisYear": 0.6292,
+  "yieldFromCreate": 0.108,
+  "windStar": 5,
+  "szStar": 5
+},{
+  "_id": {
+    "$oid": "5fc620a9194681427cd36e85"
+  },
+  "fundName": "国投瑞银策略精选",
+  "fundId": "000165.OF",
+  "fundSecKind": "灵活配置型",
+  "fundManager": "吉莉",
+  "fundCompany": "国投瑞银基金",
+  "issueDate": {
+    "$date": "2013-09-26T15:59:17Z"
+  },
+  "benchmark": "沪深300指数*55%+中债总指数*45%",
+  "curScale": 308979835.19,
+  "avgScale": 370004296.793333,
+  "lastWeekYield": -0.0189,
+  "lastMonthYield": -0.0402,
+  "lastYearYield": 0.4875,
+  "lastQuarterYield": 0.1219,
+  "yieldInThisYear": 0.4093,
+  "yieldFromCreate": 0.146,
+  "windStar": 5,
+  "szStar": 5
+},{
+  "_id": {
+    "$oid": "5fc620a9194681427cd36e86"
+  },
+  "fundName": "华泰柏瑞季季红",
+  "fundId": "000186.OF",
+  "fundSecKind": "中长期纯债型",
+  "fundManager": "罗远航",
+  "fundCompany": "华泰柏瑞基金",
+  "issueDate": {
+    "$date": "2013-11-12T15:59:17Z"
+  },
+  "benchmark": "一年期银行定期存款收益率(税后)+1%",
+  "curScale": 3734264882.99,
+  "avgScale": 2597978237.81666,
+  "lastWeekYield": 0,
+  "lastMonthYield": -0.0025,
+  "lastYearYield": 0.0328,
+  "lastQuarterYield": -0.0029,
+  "yieldInThisYear": 0.0202,
+  "yieldFromCreate": 0.0395,
+  "windStar": 5,
+  "szStar": 5
+},{
+  "_id": {
+    "$oid": "5fc620a9194681427cd36e87"
+  },
+  "fundName": "工银瑞信成长收益A",
+  "fundId": "000195.OF",
+  "fundSecKind": "灵活配置型",
+  "fundManager": "张洋",
+  "fundCompany": "工银瑞信基金",
+  "issueDate": {
+    "$date": "2013-06-25T15:59:17Z"
+  },
+  "benchmark": "沪深300指数*60%+中证全债指数*40%",
+  "curScale": 101627661.85,
+  "avgScale": 231821896.553333,
+  "lastWeekYield": -0.005,
+  "lastMonthYield": -0.0043,
+  "lastYearYield": 0.2197,
+  "lastQuarterYield": 0.1158,
+  "yieldInThisYear": 0.1673,
+  "yieldFromCreate": 0.0623,
+  "windStar": 5,
+  "szStar": 5
+},{
+  "_id": {
+    "$oid": "5fc620a9194681427cd36e88"
+  },
+  "fundName": "博时岁岁增利",
+  "fundId": "000200.OF",
+  "fundSecKind": "中长期纯债型",
+  "fundManager": "鲁邦旺",
+  "fundCompany": "博时基金",
+  "issueDate": {
+    "$date": "2013-06-25T15:59:17Z"
+  },
+  "benchmark": "银行一年期定期存款利率(税后)*1.1",
+  "curScale": 683090383.81,
+  "avgScale": 674470732.98,
+  "lastWeekYield": 0.0009,
+  "lastMonthYield": 0.0036,
+  "lastYearYield": 0.0567,
+  "lastQuarterYield": 0.008,
+  "yieldInThisYear": 0.0412,
+  "yieldFromCreate": 0.041,
+  "windStar": 5,
+  "szStar": 5
+},{
+  "_id": {
+    "$oid": "5fc620a9194681427cd36e89"
+  },
+  "fundName": "诺安泰鑫一年A",
+  "fundId": "000201.OF",
+  "fundSecKind": "中长期纯债型",
+  "fundManager": "岳帅",
+  "fundCompany": "诺安基金",
+  "issueDate": {
+    "$date": "2013-11-04T15:59:17Z"
+  },
+  "benchmark": "一年期定期存款利率(税后)+1.2%",
+  "curScale": 68343552.54,
+  "avgScale": 217007973.586666,
+  "lastWeekYield": 0.0011,
+  "lastMonthYield": -0.001,
+  "lastYearYield": 0.0405,
+  "lastQuarterYield": 0.0011,
+  "yieldInThisYear": 0.0265,
+  "yieldFromCreate": 0.0364,
+  "windStar": 5,
+  "szStar": 5
+},{
+  "_id": {
+    "$oid": "5fc620a9194681427cd36e8a"
+  },
+  "fundName": "泰信鑫益A",
+  "fundId": "000212.OF",
+  "fundSecKind": "中长期纯债型",
+  "fundManager": "何俊春",
+  "fundCompany": "泰信基金",
+  "issueDate": {
+    "$date": "2013-07-16T15:59:17Z"
+  },
+  "benchmark": "中国人民银行公布的一年期定期存款基准利率(税后)+0.5%",
+  "curScale": 72738981.93,
+  "avgScale": 64770794.9033333,
+  "lastWeekYield": 0,
+  "lastMonthYield": -0.0008,
+  "lastYearYield": 0.0437,
+  "lastQuarterYield": 0.0025,
+  "yieldInThisYear": 0.0311,
+  "yieldFromCreate": 0.0326,
+  "windStar": 5,
+  "szStar": 5
+},{
+  "_id": {
+    "$oid": "5fc620a9194681427cd36e83"
+  },
+  "fundName": "大成景兴信用债A",
+  "fundId": "000130.OF",
+  "fundSecKind": "混合债券型一级",
+  "fundManager": "王立，孙丹",
+  "fundCompany": "大成基金",
+  "issueDate": {
+    "$date": "2013-06-03T15:59:17Z"
+  },
+  "benchmark": "中债信用债总指数收益率*85%+同期银行活期存款利率(税后)*15%",
+  "curScale": 139806401.44,
+  "avgScale": 264948738.49,
+  "lastWeekYield": -0.0017,
+  "lastMonthYield": -0.0087,
+  "lastYearYield": 0.0348,
+  "lastQuarterYield": 0.0037,
+  "yieldInThisYear": 0.0204,
+  "yieldFromCreate": 0.0495,
+  "windStar": 5,
+  "szStar": 5
+},{
+  "_id": {
+    "$oid": "5fc620a9194681427cd36e84"
+  },
+  "fundName": "民生加银策略精选A",
+  "fundId": "000136.OF",
+  "fundSecKind": "灵活配置型",
+  "fundManager": "孙伟",
+  "fundCompany": "民生加银基金",
+  "issueDate": {
+    "$date": "2013-06-06T15:59:17Z"
+  },
+  "benchmark": "沪深300指数收益率*60%+中债国债总指数收益率(全价)*40%",
+  "curScale": 1730622704.26,
+  "avgScale": 1183267284.87666,
+  "lastWeekYield": -0.018,
+  "lastMonthYield": -0.018,
+  "lastYearYield": 0.62,
+  "lastQuarterYield": 0.1553,
+  "yieldInThisYear": 0.5412,
+  "yieldFromCreate": 0.1646,
+  "windStar": 5,
+  "szStar": 5
+},{
+  "_id": {
+    "$oid": "5fc620a9194681427cd36e8b"
+  },
+  "fundName": "泰信鑫益C",
+  "fundId": "000213.OF",
+  "fundSecKind": "中长期纯债型",
+  "fundManager": "何俊春",
+  "fundCompany": "泰信基金",
+  "issueDate": {
+    "$date": "2013-07-16T15:59:17Z"
+  },
+  "benchmark": "中国人民银行公布的一年期定期存款基准利率(税后)+0.5%",
+  "curScale": 18356697.38,
+  "avgScale": 64770794.9033333,
+  "lastWeekYield": 0,
+  "lastMonthYield": -0.0017,
+  "lastYearYield": 0.0389,
+  "lastQuarterYield": 0.0017,
+  "yieldInThisYear": 0.0271,
+  "yieldFromCreate": 0.0296,
+  "windStar": 5,
+  "szStar": 5
+},{
+  "_id": {
+    "$oid": "5fc620a9194681427cd36e8c"
+  },
+  "fundName": "华安年年红A",
+  "fundId": "000227.OF",
+  "fundSecKind": "中长期纯债型",
+  "fundManager": "周益鸣",
+  "fundCompany": "华安基金",
+  "issueDate": {
+    "$date": "2013-11-13T15:59:17Z"
+  },
+  "benchmark": "同期一年期定期存款收益率(税后)+1.2%",
+  "curScale": 2910821560.67,
+  "avgScale": 1106721113.89333,
+  "lastWeekYield": 0,
+  "lastMonthYield": 0,
+  "lastYearYield": 0.0804,
+  "lastQuarterYield": 0.0067,
+  "yieldInThisYear": 0.069,
+  "yieldFromCreate": 0.0496,
+  "windStar": 5,
+  "szStar": 5
+},{
+  "_id": {
+    "$oid": "5fc620a9194681427cd36e8d"
+  },
+  "fundName": "诺安稳固收益",
+  "fundId": "000235.OF",
+  "fundSecKind": "中长期纯债型",
+  "fundManager": "裴禹翔",
+  "fundCompany": "诺安基金",
+  "issueDate": {
+    "$date": "2013-08-20T15:59:17Z"
+  },
+  "benchmark": "固定业绩基准3.0%",
+  "curScale": 182237316.7,
+  "avgScale": 700267140.02,
+  "lastWeekYield": 0.001,
+  "lastMonthYield": -0.001,
+  "lastYearYield": 0.0268,
+  "lastQuarterYield": -0.001,
+  "yieldInThisYear": 0.0179,
+  "yieldFromCreate": 0.0304,
+  "windStar": 5,
+  "szStar": 5
+},{
+  "_id": {
+    "$oid": "5fc620a9194681427cd36e8e"
+  },
+  "fundName": "华安年年盈A",
+  "fundId": "000239.OF",
+  "fundSecKind": "中长期纯债型",
+  "fundManager": "孙丽娜",
+  "fundCompany": "华安基金",
+  "issueDate": {
+    "$date": "2015-02-02T15:59:17Z"
+  },
+  "benchmark": "一年期定期存款利率(税后)+1.2%",
+  "curScale": 58600104.05,
+  "avgScale": 68110535.1833333,
+  "lastWeekYield": 0,
+  "lastMonthYield": 0,
+  "lastYearYield": 0.0877,
+  "lastQuarterYield": 0.0049,
+  "yieldInThisYear": 0.0783,
+  "yieldFromCreate": 0.0318,
+  "windStar": 5,
+  "szStar": 5
+}]
+
+const testArray = [
+  {
+    name: "张三#张三",
+    age: 18,
+    math: 100,
+    chinese: 90,
+    english: 80,
+    star: 5
+  },
+  {
+    name: "李四#李四",
+    age: 18,
+    math: 95,
+    chinese: 90,
+    english: 85,
+    star: 4
+  },
+  {
+    name: "王五",
+    age: 19,
+    math: 70,
+    chinese: 87,
+    english: 90,
+    star: 3
+  },
+  {
+    name: "罗翔",
+    age: 40,
+    math: 80,
+    chinese: 100,
+    english: 90,
+    star: 5
+  },
+  {
+    name: "拿姐",
+    age: 27,
+    math: 70,
+    chinese: 80,
+    english: 90,
+    star: 4
+  },
+  {
+    name: "瑞哥",
+    age: 27,
+    math: 80,
+    chinese: 75,
+    english: 90,
+    star: 4
+  },
+  {
+    name: "督公",
+    age: 40,
+    math: 75,
+    chinese: 98,
+    english: 73,
+    star: 4
+  }
+]

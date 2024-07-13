1c46747036018b3d1661d303b749382a1b76703e
keeko <787018661@qq.com>
2024-07-13 20:26:34 +0800

    理解 infer关键字 的作用 (类型推断 or 提取类型)

--- 05typescript/00898-includes.ts
+++ 05typescript/00898-includes.ts
@@ -23,6 +23,17 @@ type Includes<T extends readonly any[], U> =
     ? Includes<rest, U> // 如果可以成功分解，递归地调用 Includes<rest, U>，继续在剩余的数组中查找类型 U。
     : false; // 如果无法成功分解（即数组 T 的长度为 0），则返回 false，表示数组 T 不包含类型 U。
 
+/*
+进一步解释上面的 `T extends [T[0], ...rest]`
+
+如果写成 T extends [T[0], ...rest]，TypeScript 并不知道 rest 的确切类型，因为它没有被明确定义。这会导致类型推断错误，或者在条件类型中无法准确地进行下一步操作。
+
+例如，假设 T 是一个数组类型 [number, string, boolean]，那么在 T extends [T[0], ...rest] 中，TypeScript 不知道 rest 的类型是什么，导致无法在条件类型中正确处理 rest 的递归。
+
+(类型推断 or 提取类型)
+在 ...rest 的位置使用 infer 允许 TypeScript 推断剩余部分 rest 的类型。也就是说，TypeScript 会尝试从 T 中推断出剩余数组的类型，并将其绑定到 rest 变量上。
+*/
+
 
 /* 
 type Includes<T extends readonly any[], U> = T extends [infer P, ...infer rest] 

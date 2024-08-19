2aa2cd73381596fdca57de5f2b639b0ca83911d1
Keeko <787018661@qq.com>
2024-08-19 09:43:15 +0800

    ts integer

---
+++ 05typescript/10969-integer.ts
@@ -0,0 +1,42 @@
+/*
+  10969 - 整数
+  -------
+  by HuaBing (@hbcraft) #中等 #template-literal
+
+  ### 题目
+
+  请完成类型 `Integer<T>`，类型 `T` 继承于 `number`，如果 `T` 是一个整数则返回它，否则返回 `never`。
+
+  > 在 Github 上查看：https://tsch.js.org/10969/zh-CN
+*/
+
+/* _____________ 你的代码 _____________ */
+
+// v1
+// type Integer<T extends number> = `${T}` extends `${bigint}` ? T : never;
+
+// v2
+type Integer<T extends number> = number extends T
+? never
+: `${T}` extends `${string}.${string}`
+  ? never
+  : T;
+
+/* 
+
+let x = 1
+let y = 1 as const
+
+type cases1 = [
+  Expect<Equal<Integer<1>, 1>>,
+  Expect<Equal<Integer<1.1>, never>>,
+  Expect<Equal<Integer<1.0>, 1>>,
+  Expect<Equal<Integer<1.000000000>, 1>>,
+  Expect<Equal<Integer<0.5>, never>>,
+  Expect<Equal<Integer<28.00>, 28>>,
+  Expect<Equal<Integer<28.101>, never>>,
+  Expect<Equal<Integer<typeof x>, never>>,
+  Expect<Equal<Integer<typeof y>, 1>>,
+]
+
+*/

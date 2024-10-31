0d590f552016a9f403ca60619d6e763b32228e44
Keeko <787018661@qq.com>
2024-10-31 10:22:59 +0800

    [feat] 数组解构效率

---
+++ singleFileCase/0413数组解构效率.js
@@ -0,0 +1,32 @@
+const arr = [11, 22, 33];
+
+// 数组形式解构
+const [arrFirst, arrSecond] = arr;
+console.log({ arrFirst, arrSecond });
+
+// 对象形式解构
+const { 0: objFirst, 1: objSecond } = arr;
+console.log({ objFirst, objSecond });
+
+const executionsCount = 100000;
+
+console.time('[]解构数组');
+for (let i = 0; i < executionsCount; i++) {
+  const [first, second] = arr;
+}
+console.timeEnd('[]解构数组');
+
+console.time('{}解构数组');
+for (let i = 0; i < executionsCount; i++) {
+  const { 0: first, 1: second } = arr;
+}
+console.timeEnd('{}解构数组');
+
+/* 
+  []解构数组: 4.277ms
+  {}解构数组: 1.616ms
+
+  在大数据量的循环里，想要对数组进行解构的话，最好是通过 {} 去解构，这样减少一点性能损耗
+
+  对数组的结构，用对象型结构的执行效率大于数组型结构的执行效率。
+*/

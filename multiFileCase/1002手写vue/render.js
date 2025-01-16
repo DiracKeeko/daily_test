7f5fef5b52d3cfb8c0078dc4012e06092bf5c3bc
keeko <787018661@qq.com>
2025-01-16 20:55:08 +0800

    [feat] 手写mount  处理attrs中的事件绑定

--- multiFileCase/1002手写vue/render.js
+++ multiFileCase/1002手写vue/render.js
@@ -14,6 +14,21 @@ function mount(vNode, container) {
   // 1 根据sel 创建真实dom元素el 并将这个el保存到vNode的el属性 (便于访问)
   const el = (vNode.el = document.createElement(vNode.sel));
 
+  
+  // 4 设置属性值
+  if (vNode.attrs) {
+    for (const key in vNode.attrs) {
+      if (key.startsWith('on') && typeof vNode.attrs[key] === 'function') {
+        // 4.2 增加对事件的特殊处理
+        const eventType = key.slice(2).toLowerCase();
+        el.addEventListener(eventType, vNode.attrs[key]);
+      } else {
+        // 4.1 设置属性值
+        el.setAttribute(key, vNode.attrs[key]);
+      }
+    }
+  }
+  
   // 3 挂载vNode中的children  递归操作
   /*
   if (vNode.children) {
@@ -31,12 +46,6 @@ function mount(vNode, container) {
     vNode.children.forEach(item => mount(item, el));
   }
 
-  // 4 设置属性值
-  if (vNode.attrs) {
-    for (const key in vNode.attrs) {
-      el.setAttribute(key, vNode.attrs[key]);
-    }
-  }
 
   // 2 挂载到container中去
   container.appendChild(el);
